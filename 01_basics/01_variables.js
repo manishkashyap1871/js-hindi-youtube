c9ecc7a3d8fed7de7b7aa18726b36460509556ea
Manish Kashyap <manishkashyap1871@gmail.com>
2026-01-31 10:52:15 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,16 @@
 const accountId = 14453
+let email="manish@gamil.com" //always use const and let for defining varibales
+var password = "1212121" //never use var because of its issue in block and functional scope
+accountCity = "Kanpur"
+let accountstate;
+
+
+/*
+accountId == 212121
+console.log(accountId)
+#constant can never be change beacuse they have fixed value
+*/
+
+
+console.table([accountId, email, password, accountstate])
+
