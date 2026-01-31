48010a939077ff7e6d9b6849d94e2de80cc147e1
Manish Kashyap <manishkashyap1871@gmail.com>
2026-01-31 12:51:40 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,14 +4,13 @@ var password = "1212121" //never use var because of its issue in block and funct
 accountCity = "Kanpur"
 let accountstate;
 
-
 /*
 accountId == 212121
 console.log(accountId)
 #constant can never be change beacuse they have fixed value
 */
 
-
-console.table([accountId, email, password, accountstate])
+console.log(email) //used to get output  
+console.table([accountId, email, password, accountstate]) //this used to get output in tabular form
 
 //use container config done : -> for saving commit changes is git repsitory
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS version as newer version
+
+//alert(3+3) // it give incorrect output as we are using nodejs node web browser
+
+let name = "Manish"
+let age = 21
+let isLoggedIn = true
+
+
+/*
+types of datatype and there range
+
+----- primitive datatype
+
+number -> 2 to power 53
+bigInt
+string -> ""
+boolean -> true or false
+null -> standalone value (empty value)
+undefined -> till now no value is assigned
+symbol -> used for uniqueness
+
+---object
+*/
+
+console.log(typeof age)
+console.log(typeof null) //gives output : object
+console.log(typeof undefined) //gives output : undefined 
+
