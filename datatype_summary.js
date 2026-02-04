0b6f628dbb11a19f5d17639777a3c265c1b1bbcc
Manish Kashyap <manishkashyap1871@gmail.com>
2026-02-04 16:45:38 +0000

    container config done

---
+++ datatype_summary.js
@@ -0,0 +1,33 @@
+//  ----------->primitive datatypes 
+
+// 7 Types --> string , number, Boolean, Null, Undefined, Symbol, BigInt
+
+// # JavaScript is a Dynamically Typed Language ✅
+// This means you do NOT need to specify the type of a variable — it is determined at runtime.
+// Example :
+// let x = 10;      // number
+// x = "hello";     // now string (no error)
+// x = true;        // now boolean (still no error)
+
+// ------------->reference (Non primtive)
+// Array , Objects, Functions
+
+// example 
+
+//--Array
+ const  heroes =["shaktiman", "spiderman", "avengers"]
+//--Object
+let myobj ={
+    name: "Manish",
+    age : 22
+}
+//--function
+const myfunction = function(){
+    console.log("hello");
+}
+
+console.log(typeof heroes); //--> object
+console.log(typeof myfunction); //-->function
+console.log(typeof myobj); //-->object
+
+
