0a4284cd91877635a9f54884ac9bdfe3671701e4
Manish Kashyap <manishkashyap1871@gmail.com>
2026-02-11 17:42:59 +0000

    function basic

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -26,17 +26,47 @@ function addtwonum(num1,num2){
 const result = addtwonum(3,5)
 // console.log("results :",result); // here we first return the reuslt in the function so it can be passed in the reuslt
 
-function loginuser(username){
+function loginuser(username /*= "sam"*/){ // here this "sam" is default value it can be used inplace of below if condition
     if(/* !username */ username===undefined){ // this if condition is used for checking whether the username is enter or not 
         console.log("please enter a username");
         return
     }
     return `${username} just logged in`
 }
+// console.log(loginuser());
 
-// console.log(loginuser("manish"));
-
-function calculationcartprice(...num1){
+//-----to handle mutiple input at a time
+function calculationcartprice(val1,val2,...num1){ //here "..." is a rest operator to handle multiple inputs at the same time
     return num1
 }
-console.log();
+//console.log(calculationcartprice(200,400,500)); //when we use val1, val2 in parameters then it staring 2 values are stored in these two and rest is return in num1
+
+
+//---------handling obejct inputs by function
+const user ={
+    username : "manish",
+    price : 199
+}
+
+function handleobject(anyobject){
+    console.log(`name is ${anyobject.username} and price is ${anyobject.price}`);    
+}
+
+// handleobject(user) //-this work on custom already exist output
+//now handling function by generating new input
+handleobject({
+    username : "adhyan",
+    price : 100
+})
+
+
+//---function to return the 2nd element of the array
+
+const myArray = [200,300,400,500,600]
+
+function returnSecondElement(getarray) {
+    return getarray[1]
+}
+
+// console.log(returnSecondElement(myArray)); 
+console.log(returnSecondElement([1000,900,800,700,600]));
