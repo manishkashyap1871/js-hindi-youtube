9548aab5cfa7f626107e84f47cee0d101f2b4dd0
Manish Kashyap <manishkashyap1871@gmail.com>
2026-02-26 11:24:37 +0000

    commit

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,31 @@
+//---------------Basic syntax of switch
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// example
+
+const month = 3
+switch (month) {
+case 1:
+    console.log("january");
+    break;
+case 2:
+    console.log("february");
+    break;
+case 3:
+    console.log("march");
+    break;
+case 4:
+    console.log("april");
+    break;
+
+    default:
+        console.log("default case match");
+        break;
+}
