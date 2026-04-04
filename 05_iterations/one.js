556bf249d1d7b047e6bf90eb9e5137575f6f75ea
Manish Kashyap <manishkashyap1871@gmail.com>
2026-04-04 10:32:44 +0000

    commit

---
+++ 05_iterations/one.js
@@ -0,0 +1,56 @@
+// ------------------for
+
+// example 1 of "for"
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if (element==5) {
+//         console.log("5 is the best number");
+//     }
+//     console.log(element);    
+// }
+// output :0
+// 0
+// 1  
+// 2
+// 3
+// 4
+// 5 is the best number
+// 5
+// 6
+// 7
+// 8
+// 9
+
+
+//example : printing mathematical tables
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`table of ${i} : `);
+//     console.log();
+//     for (let j = 0; j <=10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);        
+//     }
+//     console.log();
+    
+// }
+//-------------------example
+
+// let myarray = ["flash","batman","superman"]
+// console.log(myarray.length);
+// for (let index = 0; index < myarray.length; index++) {
+//     const element = myarray[index];
+//     console.log(element);
+// }
+
+//--------------------- break and continue
+// for(let index = 1; index<=20;index++){
+//     if(index==5){
+//         console.log(`5 index detected`);
+//         //break;
+//         continue;
+        
+//     }
+//     console.log(`value of i is ${index}`);
+    
+// }
+
